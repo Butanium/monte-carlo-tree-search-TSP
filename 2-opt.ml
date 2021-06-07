140c84050dd28d3f34d0c6b96537f1282057c637
Clement Dumas <dumasclement2002@gmail.com>
2021-06-07 22:32:33 +0200

    added 2 opt algorithm

---
+++ 2-opt.ml
@@ -0,0 +1,33 @@
+module TwoOpt = struct
+    type arg = {city_count: int; eval : int -> int -> float}
+    let make_arg city_count eval = {city_count; eval}
+    let optBest arg max arr =
+        let rec loop k =
+            let diff = ref 0. in
+            let minI, minJ = ref 0, ref 0 in
+                for i = 0 to arg.city_count - 4 do
+                    for j = i+2 to arg.city_count - 2 do
+                        let d = arg.eval arr.(i) arr.(j) +. arg.eval arr.(i+1) arr.(j+1)
+                        -. arg.eval arr.(i) arr.(i+1) -. arg.eval arr.(j) arr.(j+1)
+                        in
+                        if d < !diff then (
+                            diff := d;
+                            minI := i;
+                            minJ := j
+                        )
+                    done
+                done;
+            if !diff < 0. then (
+                let i, j = !minI, !minJ in
+                for k = 0 to (j - i)/2 - 1 do
+                    let t = arr.(i+1+k) in
+                    arr.(i+1+k) <- arr.(j-k);
+                    arr.(j-k) <- t;
+                done;
+                if k < max || max < 0 then loop (k+1)
+            )
+        in loop 1
+
+
+
+end;;
--- monte-carlo.ml
+++ monte-carlo.ml
@@ -1,13 +1,49 @@
-let city_count = 50;;
-type city = {x:float; y:float};;
-let ic = open_in "C:/Users/Clement/Documents/prépa/algogen/saves/save_1.txt";;
-let pos = Array.init city_count
-    (fun _ ->
-        let x::y::[] = List.map (fun s -> float_of_string @@ String.trim s)
-        @@ let s = input_line ic in String.split_on_char ',' @@ String.sub s 1 (String.length s - 2)
-            in {x;y}
-    );;
-let dist c1 c2 = ((c1.x -. c2.x)**2. +. (c1.y -. c2.y)**2.)**0.5;;
+module MCTS = struct
+    let mapInPlace f arr = Array.iteri (fun i x -> arr.(i) <- f x) arr
+    module IntSet = Set.Make(Int)
+    module RndQ = struct
+        exception Empty
+        type 'a t = {mutable size: int; content : 'a array; weights : float array; mutable tot : float}
+        (** [create size arr] *)
+        let getTot q = let t = ref 0 in for k = 0 to q.size - 1 do t := !t +. weights.(content.(k)) done;
+        
+        let create size arr weights = let tot = Array.fold_left (+.) 0. weights in
+                 Array.{size; content = arr; weights; tot}
+        let is_empty q = q.size = 0
+        let take t = if t.size = 0 then raise Empty else
+            let rec aux k acc = let acc = acc -. t.weights.(k) in
+                if k = t.size - 1 || acc <= 0. then k else aux (k+1) acc
+            in
+            let i = aux 0 @@ Random.float 1.  in
+            let r = t.content.(i) in
+            t.content.(i) <- t.content.(t.size - 1);
+            t.size <- t.size - 1;
+            r
+    end
+    type arg = {city_count: int; eval : int -> int -> float}
+    let playout arg score start visited len =
+        let av_count = arg.city_count - len in
+        let arr = Array.make av_count (-1) in
+        let fill = let ind = ref 0 in
+            fun x -> arr.(!ind) <- x; incr ind
+        in
 
-let adj_matrix = Array.init city_count (fun i -> Array.init city_count (fun j -> dist pos.(i) pos.(j)));;
+        let available =
+            let rec aux i accC =
+                if not @@ i = arg.city_count || accC = av_count
+                    then (
+                        let b = IntSet.mem i visited in if b then fill i;
+                        aux (i+1) (accC + if b then 0 else 1)
+                    )
+            in aux 0 0
+        in
+        RndQ.create av_count arr @@ Array.init av_count (fun i -> arg.eval arr.(i) start)
 
+
+
+
+
+
+
+
+end
