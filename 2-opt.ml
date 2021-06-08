cdeac1f85fa72f51c6f2c01f26d8b57e78415f7c
Clement Dumas <dumasclement2002@gmail.com>
2021-06-08 23:58:59 +0200

    WIP MCTS

--- 2-opt.ml
+++ 2-opt.ml
@@ -1,13 +1,18 @@
 module TwoOpt = struct
-    type arg = {city_count: int; eval : int -> int -> float}
-    let optBest city_count eval max arr =
+    let invertPath i j path =
+        for k = 0 to (j - i)/2 - 1 do
+             let t = path.(i+1+k) in
+             path.(i+1+k) <- path.(j-k);
+             path.(j-k) <- t;
+         done
+    let opt_best city_count eval max path =
         let rec loop k =
             let diff = ref 0. in
             let minI, minJ = ref 0, ref 0 in
-                for i = 0 to arg.city_count - 4 do
-                    for j = i+2 to arg.city_count - 2 do
-                        let d = arg.eval arr.(i) arr.(j) +. arg.eval arr.(i+1) arr.(j+1)
-                        -. arg.eval arr.(i) arr.(i+1) -. arg.eval arr.(j) arr.(j+1)
+                for i = 0 to city_count - 4 do
+                    for j = i+2 to city_count - 2 do
+                        let d = eval path.(i) path.(j) +. eval path.(i+1) path.(j+1)
+                        -. eval path.(i) path.(i+1) -. eval path.(j) path.(j+1)
                         in
                         if d < !diff then (
                             diff := d;
@@ -17,16 +22,22 @@ module TwoOpt = struct
                     done
                 done;
             if !diff < 0. then (
-                let i, j = !minI, !minJ in
-                for k = 0 to (j - i)/2 - 1 do
-                    let t = arr.(i+1+k) in
-                    arr.(i+1+k) <- arr.(j-k);
-                    arr.(j-k) <- t;
-                done;
+                invertPath !minI !minJ path;
                 if k < max || max < 0 then loop (k+1)
             )
         in loop 1
-
-
-
+    let opt_fast city_count eval max path =
+        let rec rec_while i = (i < max || max < 0) &&
+            not (loop1 0) && rec_while (i+1)
+        and loop1 i = i >= city_count - 4 || (loop2 i (i+2) && loop1 (i+1))
+        and loop2 i j = j >= city_count - 1 || (
+            if eval path.(i) path.(j) +. eval path.(i+1) path.(j+1)
+            -. eval path.(i) path.(i+1) -. eval path.(j) path.(j+1) < 0. then (
+                invertPath i j path;
+                Printf.printf "\ninverted %d and %d" i j;
+                false
+            ) else true
+        ) && loop2 i (j+1)
+        in
+        let _ = rec_while 0 in ()
 end;;
--- monte-carlo.ml
+++ monte-carlo.ml
@@ -1,43 +1,132 @@
 module MCTS = struct
     let mapInPlace f arr = Array.iteri (fun i x -> arr.(i) <- f x) arr
+    
+
     module IntSet = Set.Make(Int)
     module RndQ = struct
         exception Empty
         type 'a t = {mutable size: int; content : 'a array; weights : float array; mutable tot : float}
         (** [create size arr] *)
-        let getTot q = let t = ref 0 in for k = 0 to q.size - 1 do t := !t +. weights.(content.(k)) done;
-        
+        let getTot q = let t = ref 0. in for k = 0 to q.size - 1 do t := !t +. q.weights.(q.content.(k)) done;
+            !t
+
         let create size arr weights = let tot = Array.fold_left (+.) 0. weights in
-                 Array.{size; content = arr; weights; tot}
+                 {size; content = arr; weights; tot}
         let is_empty q = q.size = 0
-        let take t = if t.size = 0 then raise Empty else
-            let rec aux k acc = let acc = acc -. t.weights.(k) in
-                if k = t.size - 1 || acc <= 0. then k else aux (k+1) acc
+        let take q = if q.size = 0 then raise Empty else
+            let rec aux k acc = let acc = acc -. q.weights.(k) in
+                if k = q.size - 1 || acc <= 0. then k else aux (k+1) acc
             in
-            let i = aux 0 @@ Random.float 1.  in
-            let r = t.content.(i) in
-            t.content.(i) <- t.content.(t.size - 1);
-            t.size <- t.size - 1;
+            let i = aux  0 @@ Random.float 1.*. getTot q  in
+            let r = q.content.(i) in
+            q.content.(i) <- q.content.(q.size - 1);
+            q.size <- q.size - 1;
             r
+        let tot_empty q =
+            Array.init q.size (fun _ -> take q)
     end
-    type arg = {city_count: int; eval : int -> int -> float}
-    let playout arg score start visited len =
-        let av_count = arg.city_count - len in
-        let arr = Array.make av_count (-1) in
-        let fill = let ind = ref 0 in
-            fun x -> arr.(!ind) <- x; incr ind
+    type baseNode = {mutable accScore:float; mutable visit:float; mutable sons: mctNode list;
+            poss_cities: int RndQ.t; accDist:float; city:int}
+    and herit_node = R of {used_cities: IntSet.t; init_score: float; init_length: int} | N of mctNode
+    and mctNode = {base: baseNode; herit_node: herit_node}
+    type creation_mode = Roulette | Random
+    type opt_mode = No_opt | Two_Opt_Best of int | Two_Opt_Fast of int
+    type select_mode = Ucb1 of float
+    type arg = {city_count: int; eval : int -> int -> float; rnd_creation_mode: creation_mode; opt_mode: opt_mode;
+                mutable score:float; start:int; mutable length:int; mutable visited:IntSet.t; mutable last_city:int;
+                select_mode: select_mode}
+    let get_father node = match node.herit_node with | R _ -> failwith "no dads for root" | N n -> n
+    let node_score arg =
+        match arg.select_mode with
+        | Ucb1 e -> fun node -> let nf = (get_father node).base.visit in
+                                e *. (log(nf) /. node.base.visit) ** 0.5  -. node.base.accScore /. node.base.visit
+    let random_creation arg =
+        match arg.rnd_creation_mode with
+        | Roulette -> arg.eval arg.last_city
+        | Random -> fun _ -> 1.
+        | _ -> failwith "not implemented"
+    let playout_opt arg =
+        match arg.opt_mode with
+        | No_opt -> fun _ -> ()
+        | Two_Opt_Best m -> TwoOpt.opt_best arg.city_count arg.eval m
+        | Two_Opt_Fast m -> TwoOpt.opt_fast arg.city_count arg.eval m
+   
+    let available arg =
+       let av_count = arg.city_count - arg.length
+       in 
+       let arr = Array.make av_count (-1) in
+       let fill = let ind = ref 0 in
+           fun x -> arr.(!ind) <- x; incr ind
+       in
+       let rec aux i accC =
+           if not (i = arg.city_count || accC = av_count)
+               then (
+                   let b = IntSet.mem i arg.visited in if b then fill i;
+                   aux (i+1) (accC + if b then 0 else 1)
+               )
+       in aux 0 0;
+       RndQ.create av_count arr @@ Array.init av_count (fun i -> random_creation arg arr.(i))
+    let playout arg =
+        let av_count = arg.city_count - arg.length in
+        let q = available arg in
+        let end_path = RndQ.tot_empty q
         in
-
-        let available =
-            let rec aux i accC =
-                if not @@ i = arg.city_count || accC = av_count
-                    then (
-                        let b = IntSet.mem i visited in if b then fill i;
-                        aux (i+1) (accC + if b then 0 else 1)
-                    )
-            in aux 0 0
+        playout_opt arg end_path;
+        let r = ref @@ arg.score +. eval arg.last_city end_path.(0) +. eval end_path.(av_count - 1) arg.start
         in
-        RndQ.create av_count arr @@ Array.init av_count (fun i -> arg.eval arr.(i) start)
+        for i = 1 to av_count - 2 do
+            r := !r +. eval end_path.(i) end_path.(i+1)
+        done;
+        !r
+    let rec retropropagation node value =
+        node.base.visit <- node.base.visit +. 1.;
+        node.base.accScore <- value +. node.base.accScore;
+        match node.herit_node with
+            | N father -> retropropagation father value
+            | _ -> ()
+
+    let expend node arg =
+        try (
+        let x = RndQ.take node.base.poss_cities in
+            let dist = arg.eval node.base.city x
+            in
+            arg.score <- arg.score +. dist;
+            arg.length <- arg.length + 1;
+            arg.visited <- IntSet.add x arg.visited;
+            arg.last_city <- x;
+            let newN = {base={accScore=0.; visit=0.; sons = []; poss_cities = available arg;
+                    accDist = node.base.accDist +. dist; city=x}; herit_node = N node}
+            in
+            node.base.sons <- newN :: node.base.sons;
+            retropropagation newN @@ playout arg
+        ) with Failure e -> failwith @@ "expend failed : "^e
+    
+    let rec select node arg =
+        match RndQ.is_empty node.base.poss_cities, node.base.sons with 
+            | true, [] -> retropropagation node @@ arg.score +. arg.eval node.base.city arg.last_city
+            | false, _ -> expend node arg
+            | true, x :: xs -> let score = node_score arg in
+                let _, n = List.fold_left
+                     (fun ((accS, accN) as acc) n -> let s =  score n in if s > accS then s, n else acc) (score  x, x) xs
+                     in select n arg
+    let mcts city_count eval rnd_creation_mode opt_mode select_mode min_conv min_playouts max_time =
+        let arg = {city_count; eval; rnd_creation_mode; opt_mode; score = 0.; start = 0; length = 1;
+            visited = IntSet.singleton 0; last_city=0; select_mode}
+        in
+        let poss_cities = available arg in
+        in
+        let root = ref {base={accScore=0.; visit = 0.; sons = []; poss_cities; accDist = 0.; city=0};
+            herit_node = R{used_cities = IntSet.singleton 0; init_score=0.; init_length=1}}
+        in
+        let getRatio() =
+            let r = List.fold_left (fun acc n -> max acc n.visit) (!root).base.sons
+            in r /. (!root.base.visit)
+        in
+        while (match !root.herit_node with | R {init_length;_} -> init_length | _ -> failwith "Invalid root") < city_count do
+            let t = Sys.time() in
+            while (!root).base.visit < float_of_int min_playouts && getRatio() < min_conv && Sys.time() -. t < max_time do
+                (*reset args *)
+                select !root arg;
 
 
 
@@ -46,4 +135,9 @@ module MCTS = struct
 
 
 
-end
+
+
+
+
+
+end;;
