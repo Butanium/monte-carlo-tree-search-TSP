02b56dc27e5b0c42c9e91d126e3adcfe3029b529
Clement Dumas <dumasclement2002@gmail.com>
2022-01-14 17:45:53 +0100

    fixed typo

--- tsp_modules/Reader_tsp.ml
+++ tsp_modules/Reader_tsp.ml
@@ -24,22 +24,22 @@ let open_tsp ?(file_path = "tsp_instances") tsp_name =
     city_count, cities
 
 let open_path ?(file_path = "tsp_instances") tsp_name =
-let city_count = Scanf.sscanf tsp_name "%[^0-9]%d" (fun _ c -> c)
-in
-let path = Array.make city_count 0
-in
-let fill = let i = ref 0 in
-    fun x -> path.(!i) <- x-1; incr i
-in
-let ic = open_in  @@
-Printf.sprintf "%s/%s.opt.tour" file_path tsp_name
-in
-let rec loop started = try (let s = String.trim @@ input_line ic in
-    if started then (
-        List.iter fill @@ List.map int_of_string @@ String.split_on_char ' ' s;
-        loop true
-    ) else loop ("TOUR_SECTION" = s)
-    ) with _ -> ();
-in loop false;
-path
+    let city_count = Scanf.sscanf tsp_name "%[^0-9]%d" (fun _ c -> c)
+    in
+    let path = Array.make city_count 0
+    in
+    let fill = let i = ref 0 in
+        fun x -> path.(!i) <- x-1; incr i
+    in
+    let ic = open_in  @@
+    Printf.sprintf "%s/%s.opt.tour" file_path tsp_name
+    in
+    let rec loop started = try (let s = String.trim @@ input_line ic in
+        if started then (
+            List.iter fill @@ List.map int_of_string @@ String.split_on_char ' ' s;
+            loop true
+        ) else loop ("TOUR_SECTION" = s)
+        ) with _ -> ();
+    in loop false;
+    path
 
