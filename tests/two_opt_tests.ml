78237c5c9bf99b6704cc111afdeb60581a03a679
Clement Dumas <dumasclement2002@gmail.com>
2022-02-13 13:36:23 +0100

    added a test for two_opt crash

--- tests/two_opt_tests.ml
+++ tests/two_opt_tests.ml
@@ -2,7 +2,7 @@
   dune exec ./tests/two_opt_main.exe
 *)
 
-let city_config = "att48"
+let city_config = "TSP100/instance2"
 
 let city_count, cities = Reader_tsp.open_tsp city_config
 
@@ -17,7 +17,7 @@ let max_time = 10.
 let max_try = max_int
 (* let path = Two_Opt.iter_two_opt adj_matrix city_count Two_Opt.Roulette max_time max_try *)
 
-let show () =
+(* let show () =
   let len = Base_tsp.path_length adj_matrix path in
   let best_len = Base_tsp.best_path_length city_config adj_matrix in
   Printf.printf "\n%% of error : %.2f %%\n"
@@ -29,4 +29,7 @@ let () =
   show ();
   Two_Opt.opt_fast ~partial_path:true ~lower_bound:5 ~upper_bound:35 adj_matrix
     path;
-  show ()
+  show () *)
+
+let crash_test = Two_Opt.(iter_two_opt adj_matrix city_count ~seed:386772978 Random 5000. max_int 
+  ~logs_path:"logs/crash_test_2opt")
