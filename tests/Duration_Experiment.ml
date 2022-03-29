7ba16a599becf8350820a054d30d915207926482
Clement Dumas <dumasclement2002@gmail.com>
2022-03-29 18:16:12 +0200

    new durations

--- tests/Duration_Experiment.ml
+++ tests/Duration_Experiment.ml
@@ -1,9 +1,9 @@
-let durations = [ 0.1; 0.25; 0.5; 1.; 2. ]
-
-let linux_duration = List.map (fun x -> x *. 60.) durations
+(* let durations = [ 0.01; 0.05; 0.1; 0.25; 0.5; 1.; 2. ] *)
+let durations = [ 0.01; 0.05 ]
+(* let linux_duration = List.map (fun x -> x *. 60.) durations *)
 
 let () =
-  let durations = if Sys.os_type == "Unix" then linux_duration else durations in
+  (* let durations = if Sys.os_type == "Unix" then linux_duration else durations in *)
   List.iter
     (fun max_time ->
       Experiments.Experiment.experiment_all ~max_time ~amount:50 ~test_set:50 ())
