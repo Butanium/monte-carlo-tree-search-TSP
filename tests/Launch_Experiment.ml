f4f6bbf75c7eed2113f8d025eaab0bc428bc4ef8
Clement Dumas <dumasclement2002@gmail.com>
2022-05-01 21:57:28 +0200

    added iterated 2opt experiment

--- tests/Launch_Experiment.ml
+++ tests/Launch_Experiment.ml
@@ -1,1 +1,11 @@
-let () = Experiments.Experiment_Generator.experiment_all ()
+(*
+   - For all experiments :
+    let () = Experiments.Experiment_Generator.experiment_all ()
+*)
+
+(*
+    - For iterated 2-Opt experiments :
+*)
+let () =
+  Experiments.Experiment_Generator.experiment_iter2opt ~max_time:10. ~amount:128
+    ~test_set:100 ~exp_per_config:5 ()
